b0eb4642cf7e314439137c2212d8082854bae181
LeeSeungkyu <zx6658@lieklion.org>
2017-11-13 23:33:58 +0900

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,82 +10,103 @@ export default class Contact extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedkey: -1,
+            selectedKey: -1,
             keyword: '',
+            nextId: 4,
             contactData: [{
+                id: 0,
                 name: 'Abet',
                 phone: '010-0000-0001'
             }, {
+                id: 1,
                 name: 'Betty',
                 phone: '010-0000-0002'
             }, {
+                id: 2,
                 name: 'Charlie',
                 phone: '010-0000-0003'
             }, {
+                id: 3,
                 name: 'David',
                 phone: '010-0000-0004'
             }]
         };
-        this.handleChange=this.handleChange.bind(this);
-        this.handleClick =this.handleClick.bind(this);
 
-        this.handleCreate =this.handleCreate.bind(this);
-        this.handleRemove =this.handleRemove.bind(this);
-        this.handleEdit =this.handleEdit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+
+        this.handleCreate = this.handleCreate.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+
     }
-    handleChange(e){
-      this.setState ({
-        keyword: e.target.value
-      });
-    }
-    handleClick(key){
-      this.setState({
-        selectedkey: key
-      });
-      console.log(key, 'is selected');
-    }
-    handleCreate(contact){
-      this.setState({
-        contactData: update(this.state.contactData, { $push: [contact]})
-      });
-    }
-    handleRemove(){
-        if(this.state.selectedkey < 0){
-          return;
-        }
+  
+
+
+
+    handleChange(e) {
         this.setState({
-          contactData: update(this.state.contactData,
-            { $splice: [[this.state.selectedkey,1]]}
-        ),
-        selectedkey: -1
-      });
+            keyword: e.target.value
+        });
     }
-    handleEdit(name, phone){
-      this.setState({
-        contactData: update(this.state.contactData,
-            {
-              [this.state.selectedkey]:{
-                name: {$set: name},
-                phone: {$set: phone}
-              }
-            }
-        )
-      });
+
+    handleClick(id) {
+        this.setState({
+            selectedId: id
+        });
+
+        console.log(id, 'is selected');
+    }
+
+    handleCreate(contact) {
+        contact.id = this.state.nextId;
+
+        this.setState({
+            contactData: update(this.state.contactData, { $push: [contact] }),
+            nextId: this.state.nextId + 1
+        });
+    }
+
+    handleRemove() {
+        if(this.state.selectedId < 0) {
+            return;
+        }
+
+        this.setState({
+            contactData: update(this.state.contactData,
+                { $splice: [[this.state.selectedId, 1]] }
+            ),
+            selectedId: -1
+        });
+    }
+
+    handleEdit(name, phone) {
+        this.setState({
+            contactData: update(this.state.contactData,
+                {
+                    [this.state.selectedId]: {
+                        name: { $set: name },
+                        phone: { $set: phone }
+                    }
+                }
+            )
+        });
     }
 
     render() {
         const mapToComponents = (data) => {
-          data.sort((a,b)=> {return a.name>b.name;});
-          data = data.filter(
-            (contact) => {
-              return contact.name.toLowerCase().indexOf(this.state.keyword.toLowerCase()) > -1;
-            }
-          )
+            data = data.slice().sort((a,b) => a.name > b.name);
+            data = data.filter(
+                (contact) => {
+                    return contact.name.toLowerCase()
+                        .indexOf(this.state.keyword.toLowerCase()) > -1;
+                }
+            );
             return data.map((contact, i) => {
                 return (<ContactInfo
                             contact={contact}
                             key={i}
-                            onClick={() => this.handleClick(i)}/>);
+                            onClick={() => this.handleClick(contact.id)}/>);
             });
         };
 
@@ -93,19 +114,22 @@ export default class Contact extends React.Component {
             <div>
                 <h1>Contacts</h1>
                 <input
-                      name="keyword"
-                      placeholder="Search"
-                      value={this.state.keyword}
-                      onChange={this.handleChange}
-                 />
+                    name="keyword"
+                    placeholder="Search"
+                    value={this.state.keyword}
+                    onChange={this.handleChange}
+                />
                 <div>{mapToComponents(this.state.contactData)}</div>
-                <ContactDetails isSelected={this.state.selectedkey != -1}
-                                contact={this.state.contactData[this.state.selectedkey]}
-                                onRemove={this.handleRemove}
-                                onEdit={this.handleEdit}/>
+                <ContactDetails
+                    isSelected={this.state.selectedId != -1}
+                    contact={this.state.contactData[this.state.selectedId]}
+                    onRemove={this.handleRemove}
+                    onEdit={this.handleEdit}
+                />
                 <ContactCreate
-                onCreate={this.handleCreate}/>
-            </div>
+                    onCreate={this.handleCreate}
+                />
+        </div>
         );
     }
 }
